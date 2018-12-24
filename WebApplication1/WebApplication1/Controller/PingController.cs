d33312a9a0545987348d71cbc8e9f868ac4e9ebf
Abhishek Mangutkar <31777053+amangutkar@users.noreply.github.com>
2018-12-24 15:25:45 +0530

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controller/PingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PingWebAPI.Controller
+{
+    //[HttpGet]
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/ping")]
+        public ActionResult Ping()
+        {
+            return Ok("Pong..!!");
+        }
+    }
+}
